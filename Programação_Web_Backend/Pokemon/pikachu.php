c1e693da3981d6b63984530e2e644c30e1e90d41
Rafael Brecci de Souza <fbrecci@gmail.com>
2025-10-10 16:27:03 -0300

    Add files via upload

---
+++ Programação_Web_Backend/Pokemon/pikachu.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokemon</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <div class="title">
+            <h1>Escolha seu</h1>
+            <h2>Pokemon</h2>
+        </div>
+        <div class="span"></div>
+        <div class="span2">
+            <img src="img/seta.png" alt="seta" class="seta1" onclick="location.href='squirtle.php'">
+            <div class="content-container">
+                <div class="content">
+                    <img src="img/pikachu.png" alt="">
+                    <h1 class="pikachu">Pikachu</h1>
+                </div>
+            </div>
+            <img src="img/seta.png" alt="seta" class="seta2" onclick="location.href='charmander.php'">
+        </div>
+    </main>
+</body>
+</html>
